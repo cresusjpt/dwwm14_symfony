770911b61a49fd26d4c3a4788c171539341541d1
Jean-Paul Tossou <tossoujeanpaul641@gmail.com>
2022-02-02 11:57:52 +0100

    les différents niveaux d'application de l'acces control

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,10 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-
-
 
 
 //.yaml
@@ -18,8 +16,13 @@ use Symfony\Component\Routing\Annotation\Route;
 // Admin
 // client
 // clientvip
-
 //client
+
+
+
+
+
+
 class HomeController extends AbstractController
 {
 
@@ -28,23 +31,29 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'Home Function',
         ]);
     }
 
     #[Route('/hello', name: 'hello')]
+    /**
+     * @IsGranted("ROLE_CLIENT_VIP")
+     */
     public function helll(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'Hello Function',
         ]);
     }
 
     #[Route('/world', name: 'world')]
     public function world(): Response
     {
+        var_dump('Un message avant la restriction');
+
+        $this->denyAccessUnlessGranted("ROLE_CLIENT_VIP");
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'World Function',
         ]);
     }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/stock/product')]
+
 class ProductController extends AbstractController
 {
     #[Route('/', name: 'product_index', methods: ['GET'])]
